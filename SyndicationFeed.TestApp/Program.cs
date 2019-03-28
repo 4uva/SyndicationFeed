a590a53e61e7d48a636df2bc2aea6980f13443c8
4uva <41539104+4uva@users.noreply.github.com>
2019-03-28 23:34:54 +0200

    tested all the SDK APIs in the example app

--- SyndicationFeed.TestApp/Program.cs
+++ SyndicationFeed.TestApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SyndicationFeed.Common.Models;
 using SyndicationFeed.SDK;
@@ -19,11 +20,68 @@ namespace SyndicationFeed.TestApp
             DisplayCollections(initialCollections);
 
             Console.WriteLine("Adding a sample collection");
-            await root.AddCollection("Sample");
+            var coll1 = await root.AddCollection("Sample");
+            var id1 = coll1.Id;
+            Console.WriteLine("Obtained back collection:");
+            DisplayCollection(coll1);
+
+            Console.WriteLine("Adding another collection");
+            var coll2 = await root.AddCollection("Sample 2");
+            var id2 = coll2.Id;
+            Console.WriteLine("Obtained back collection:");
+            DisplayCollection(coll2);
+
+            Console.WriteLine("Deleting first collection");
+            await root.DeleteCollection(id1);
+
+            Console.WriteLine("Obtaining ids");
+            var ids = await root.GetCollectionIds();
+
+            if (!ids.Contains(id2))
+                Console.WriteLine("Unexpected: all indices don't contain added index");
+            if (ids.Contains(id1))
+                Console.WriteLine("Unexpected: all indices contain deleted index");
 
             var updatedCollections = await root.GetAllCollections();
             Console.WriteLine("Updated collections: ");
             DisplayCollections(updatedCollections);
+
+            Console.WriteLine("Getting collections one by one:");
+            foreach (var id in ids)
+            {
+                var coll = await root.GetCollection(id);
+                DisplayCollection(coll);
+            }
+
+            // now, coll2 should be present
+            Console.WriteLine("Adding feed 1");
+            var feed1 = await coll2.AddFeed(
+                FeedType.Rss, new Uri("https://blogs.microsoft.com/feed/"));
+            Console.WriteLine("Obtained back:");
+            DisplayFeed(feed1);
+
+            Console.WriteLine("Adding feed 2");
+            var feed2 = await coll2.AddFeed(
+                FeedType.Rss, new Uri("https://educationblog.microsoft.com/feed"));
+            Console.WriteLine("Obtained back:");
+            DisplayFeed(feed2);
+
+            Console.WriteLine("Adding feed 3");
+            var feed3 = await coll2.AddFeed(
+                FeedType.Rss, new Uri("https://www.reddit.com/r/microsoft/.rss?format=xml"));
+            Console.WriteLine("Obtained back:");
+            DisplayFeed(feed3);
+
+            Console.WriteLine("Removing feed 1");
+            await coll2.DeleteFeed(feed1.Id);
+
+            Console.WriteLine("Obtaining syndicated feed");
+            var commonFeed = await coll2.GetSyndicatedFeed();
+            Console.WriteLine("Obtained:");
+            DisplayFeed(commonFeed);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(false);
         }
 
         static void DisplayCollections(IEnumerable<Collection> colls)
@@ -35,6 +93,36 @@ namespace SyndicationFeed.TestApp
         static void DisplayCollection(Collection coll)
         {
             Console.WriteLine($"collection {coll.Name}, Id = {coll.Id}");
+        }
+
+        static void DisplayFeed(Feed feed)
+        {
+            Console.WriteLine($"feed {feed.Id}, {feed.Type}, has {feed.Publications.Count} publications");
+            foreach (var pub in feed.Publications.Take(10))
+                DisplayPublication(pub);
+            if (feed.Publications.Count > 10)
+                Console.WriteLine("[rest skipped]");
+        }
+
+        static void DisplayPublication(Publication publication)
+        {
+            Console.WriteLine($"publication {publication.Title}");
+
+            var summary = publication.Summary;
+            if (summary != null)
+            {
+                if (summary.Length > 40)
+                    summary = summary.Substring(0, 40) + "...";
+                Console.WriteLine($"summary: {summary}");
+            }
+
+            var content = publication.Content;
+            if (content != null)
+            {
+                if (content.Length > 80)
+                    content = content.Substring(0, 80) + "...";
+                Console.WriteLine($"content: {content}");
+            }
         }
     }
 }
