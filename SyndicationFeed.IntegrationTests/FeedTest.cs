8b80ee061eeedb9d27354197482d0fbbe9f55b7d
4uva <41539104+4uva@users.noreply.github.com>
2019-03-30 01:06:41 +0200

    finished feed tests

--- SyndicationFeed.IntegrationTests/FeedTest.cs
+++ SyndicationFeed.IntegrationTests/FeedTest.cs
@@ -76,58 +76,40 @@ namespace SyndicationFeed.IntegrationTests
                 Assert.Contains(totalFeed.Publications, p => PublicationsEqual(p, publication));
         }
 
-        //[Fact]
-        //public async Task TestCollectionId()
-        //{
-        //    var name = "Test 2";
-        //    var idBefore = await root.GetCollectionIds();
-        //    var newCollection = await root.AddCollection(name);
-        //    var idAfter = await root.GetCollectionIds();
-        //    Assert.Contains(newCollection.Id, idAfter);
-        //    Assert.DoesNotContain(newCollection.Id, idBefore);
-        //}
-        //
-        //[Fact]
-        //public async Task TestCollectionDelete()
-        //{
-        //    var coll1 = await root.AddCollection("Sample 1");
-        //    var id1 = coll1.Id;
-        //
-        //    var coll2 = await root.AddCollection("Sample 2");
-        //    var id2 = coll2.Id;
-        //
-        //    await root.DeleteCollection(id1);
-        //
-        //    var ids = await root.GetCollectionIds();
-        //
-        //    Assert.Contains(id2, ids);
-        //    Assert.DoesNotContain(id1, ids);
-        //}
-        //
-        //[Fact]
-        //public async Task TestCollectionGetAll()
-        //{
-        //    for (int i = 0; i < 5; i++)
-        //        await root.AddCollection($"Sample {i}");
-        //
-        //    List<SdkCollection> collections = await root.GetAllCollections();
-        //
-        //    List<long> ids = await root.GetCollectionIds();
-        //
-        //    foreach (long id in ids)
-        //    {
-        //        SdkCollection collection = await root.GetCollection(id);
-        //        Assert.Contains(collections, c => c.Id == collection.Id && c.Name == collection.Name);
-        //    }
-        //}
-        //
-        //[Fact]
-        //public async Task TestWrongIdAccessThrows()
-        //{
-        //    var ids = await root.GetCollectionIds();
-        //    var nonexistingId = ids.Count > 0 ? ids.Max() + 1 : 0;
-        //    await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
-        //        async () => await root.GetCollection(nonexistingId));
-        //}
+        [Fact]
+        public async Task TestFeedId()
+        {
+            var idBefore = await collection.GetFeedIds();
+            var newFeed = await collection.AddFeed(FeedType.Rss, RssFeedAddress);
+            var idAfter = await root.GetCollectionIds();
+            Assert.Contains(newFeed.Id, idAfter);
+            Assert.DoesNotContain(newFeed.Id, idBefore);
+        }
+
+        [Fact]
+        public async Task TestFeedDelete()
+        {
+            var feed1 = await collection.AddFeed(FeedType.Rss, RssFeedAddress);
+            var id1 = feed1.Id;
+        
+            var feed2 = await collection.AddFeed(FeedType.Atom, AtomFeedAddress);
+            var id2 = feed2.Id;
+        
+            await collection.DeleteFeed(id1);
+        
+            var ids = await collection.GetFeedIds();
+        
+            Assert.Contains(id2, ids);
+            Assert.DoesNotContain(id1, ids);
+        }
+        
+        [Fact]
+        public async Task TestWrongIdAccessThrows()
+        {
+            var ids = await collection.GetFeedIds();
+            var nonexistingId = ids.Count > 0 ? ids.Max() + 1 : 0;
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+                async () => await collection.GetFeed(nonexistingId));
+        }
     }
 }
