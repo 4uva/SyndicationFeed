306ad8160b3756cae0dc362c3cf3a8aa119c7beb
4uva <41539104+4uva@users.noreply.github.com>
2019-04-05 01:13:39 +0300

    added migration

---
+++ SyndicationFeed/Migrations/20190404220759_RemovedLastDownloadTimeAndValidTill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SyndicationFeed.Server.Migrations
+{
+    public partial class RemovedLastDownloadTimeAndValidTill : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastDownloadTime",
+                table: "FeedWithDownloadTime");
+
+            migrationBuilder.DropColumn(
+                name: "ValidTill",
+                table: "FeedWithDownloadTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastDownloadTime",
+                table: "FeedWithDownloadTime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidTill",
+                table: "FeedWithDownloadTime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
