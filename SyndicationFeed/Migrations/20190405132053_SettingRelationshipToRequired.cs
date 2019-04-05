a764b34763387f2580265519ba3973f3606fc772
4uva <41539104+4uva@users.noreply.github.com>
2019-04-05 16:44:21 +0300

    added database migration

---
+++ SyndicationFeed/Migrations/20190405132053_SettingRelationshipToRequired.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SyndicationFeed.Server.Migrations
+{
+    public partial class SettingRelationshipToRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedWithDownloadTime_Collections_CollectionWithFeedsId",
+                table: "FeedWithDownloadTime");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CollectionWithFeedsId",
+                table: "FeedWithDownloadTime",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedWithDownloadTime_Collections_CollectionWithFeedsId",
+                table: "FeedWithDownloadTime",
+                column: "CollectionWithFeedsId",
+                principalTable: "Collections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedWithDownloadTime_Collections_CollectionWithFeedsId",
+                table: "FeedWithDownloadTime");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CollectionWithFeedsId",
+                table: "FeedWithDownloadTime",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedWithDownloadTime_Collections_CollectionWithFeedsId",
+                table: "FeedWithDownloadTime",
+                column: "CollectionWithFeedsId",
+                principalTable: "Collections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
