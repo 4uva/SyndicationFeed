45f54502e00707de5f96ca628fd96a54673e5685
4uva <41539104+4uva@users.noreply.github.com>
2019-04-11 16:31:09 +0300

    moved logininfo and registerinfo to common classes in order to include in API

--- SyndicationFeed.Common/Models/RegisterInfo.cs
+++ SyndicationFeed.Common/Models/RegisterInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SyndicationFeed.Models
+namespace SyndicationFeed.Common.Models
 {
     public class RegisterInfo
     {
