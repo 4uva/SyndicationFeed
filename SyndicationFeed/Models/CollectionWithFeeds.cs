e818abcfd96dfa01925eddf3732ea8cd60171b97
4uva <41539104+4uva@users.noreply.github.com>
2019-04-12 17:11:00 +0300

    added User inverse mavigation property to CollectionWithFeeds

--- SyndicationFeed/Models/CollectionWithFeeds.cs
+++ SyndicationFeed/Models/CollectionWithFeeds.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using SyndicationFeed.Common.Models;
 
 namespace SyndicationFeed.Models
@@ -13,13 +14,15 @@ namespace SyndicationFeed.Models
      *     Id               +                  + (PK)
      *    Name              +                  +
      *    Feeds             -                 nav
+     *    User              -                 nav
      */
 
     [DataContract]
     public class CollectionWithFeeds : Collection
     {
-        // not setting [DataMember] in order to avoid Feeds transfer
+        // not setting [DataMember] in order to avoid Feeds and User transfer
         // through the API
         public List<FeedWithDownloadTime> Feeds { get; set; }
+        public IdentityUser User { get; set; }
     }
 }
