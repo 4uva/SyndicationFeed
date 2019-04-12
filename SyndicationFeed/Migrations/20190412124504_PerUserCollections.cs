bc8cddbb5fcb673117762a8f0e8e31a39809cf28
4uva <41539104+4uva@users.noreply.github.com>
2019-04-12 17:10:12 +0300

    added database migration for changed db model

---
+++ SyndicationFeed/Migrations/20190412124504_PerUserCollections.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SyndicationFeed.Server.Migrations
+{
+    public partial class PerUserCollections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Collections",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collections_UserId",
+                table: "Collections",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Collections_AspNetUsers_UserId",
+                table: "Collections",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Collections_AspNetUsers_UserId",
+                table: "Collections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Collections_UserId",
+                table: "Collections");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Collections");
+        }
+    }
+}
