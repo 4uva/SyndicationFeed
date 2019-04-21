1ac9b70ee673524a5410455ed1c5597c4986baa5
4uva <41539104+4uva@users.noreply.github.com>
2019-04-22 00:19:56 +0300

    added publication viewmodel

---
+++ SyndicationFeed.Client/VM/PublicationVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SyndicationFeed.Common.Models;
+
+namespace SyndicationFeed.Client.VM
+{
+    class PublicationVM : VM
+    {
+        public PublicationVM(Publication publication)
+        {
+            this.publication = publication;
+        }
+
+        readonly Publication publication;
+
+        public string Id => publication.Id;
+        public string Title => publication.Title;
+        public string Summary => publication.Summary;
+        public DateTime? PublishTime => publication.PublishTime;
+        public string Content => publication.Content;
+        public Uri Link => publication.Link;
+    }
+}
