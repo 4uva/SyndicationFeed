c2d581d8edefed0ba7cbcc2ea5396224aeeb1a10
4uva <41539104+4uva@users.noreply.github.com>
2019-04-22 00:20:33 +0300

    added loading/error and publication list to FeedVM

--- SyndicationFeed.Client/VM/FeedVM.cs
+++ SyndicationFeed.Client/VM/FeedVM.cs
@@ -13,22 +13,70 @@ namespace SyndicationFeed.Client.VM
         public FeedVM(SdkCollection modelCollection, long id)
         {
             this.id = id;
-            var t = Load(modelCollection, id);
+            _ = Load(modelCollection, id);
         }
 
         long id;
         Feed feed;
-        string Error;
+
+        bool isLoading;
+        public bool IsLoading
+        {
+            get => isLoading;
+            private set => Set(ref isLoading, value);
+        }
+
+        string error;
+        public string Error
+        {
+            get => error;
+            set => Set(ref error, value);
+        }
+
+        IReadOnlyCollection<PublicationVM> publications;
+        public IReadOnlyCollection<PublicationVM> Publications
+        {
+            get => publications;
+            private set
+            {
+                if (Set(ref publications, value))
+                    PublicationCount = publications?.Count ?? 0;
+            }
+        }
+
+        int publicationCount;
+        public int PublicationCount
+        {
+            get => publicationCount;
+            private set => Set(ref publicationCount, value);
+        }
 
         async Task Load(SdkCollection modelCollection, long id)
         {
             try
             {
+                IsLoading = true;
+                Error = null;
+                Publications = null;
                 feed = await modelCollection.GetFeed(id);
+                if (feed.LoadFailureMessage != null)
+                {
+                    Error = feed.LoadFailureMessage;
+                    Publications = null;
+                }
+                else
+                {
+                    Error = null;
+                    Publications = feed.Publications.Select(p => new PublicationVM(p)).ToList();
+                }
             }
             catch (Exception ex)
             {
                 Error = "Cannot download feed";
+            }
+            finally
+            {
+                IsLoading = false;
             }
         }
 
